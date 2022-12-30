2f7096ff85e126e0ebfa701f0e67ee878b22ed23
vorlefan <dax-soft@live.com>
2022-12-30 16:13:13 -0300

    reinstall

--- client/components/file/styles/index.ts
+++
@@ -1,4 +0,0 @@
-import { styled } from '@mui/material';
-import { THEME, CONSTANTS } from '$/styles/index';
-
-export const Root = styled('div')(({ theme, ...props }) => ({}));
--- next.config.js
+++ next.config.js
@@ -1,33 +1,13 @@
-
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-    unstable_flexsearch: true
-})
-
-/**
- * @type {import('next').NextConfig}
- */
-
-const nextConfig = {
-    compiler: {
-        emotion: true,
-    },
-    // reactStrictMode: true,
-    poweredByHeader: false,
-    images: {
-    },
-    compress: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: {
-        domains: ['localhost', '*','vorlefan.com'],
-        formats: ['image/avif', 'image/webp'],
-        
-    }, 
-   
-};
-
-
-module.exports = withNextra(nextConfig);
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+    staticImage: true,
+    latex: true,
+    flexsearch: {
+      codeblock: false
+    }
+  })
+  
+  module.exports = withNextra({
+    reactStrictMode: true
+  })
